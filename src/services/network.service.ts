38eddc3f00eb695d11ebfb5ed7c174897c279e64
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-06-22 14:39:23 +0200

    move raw upload details to network service

--- src/services/network.service.ts
+++ src/services/network.service.ts
@@ -1,30 +1,78 @@
 import downloadFile from "../network/download";
+import { uploadFile } from "../network/upload";
 
 interface NetworkCredentials {
   user: string;
   pass: string;
 }
 
+function getSendAccountParameters(): {
+  bucketId: string,
+  user: string,
+  pass: string,
+  encryptionKey: string
+} {
+  return {
+    bucketId: process.env.REACT_APP_SEND_BUCKET_ID,
+    user: process.env.REACT_APP_SEND_USER,
+    pass: process.env.REACT_APP_SEND_PASS,
+    encryptionKey: process.env.REACT_APP_SEND_ENCRYPTION_KEY
+  }
+}
+
 export class NetworkService {
   private constructor(private readonly creds: NetworkCredentials) {}
   public static getInstance(): NetworkService {
+    const accountParams = getSendAccountParameters();
+
     return new NetworkService({
-      user: process.env.REACT_APP_SEND_USER,
-      pass: process.env.REACT_APP_SEND_PASS
+      user: accountParams.user,
+      pass: accountParams.pass
     });
   }
 
-  public getDownloadFileStream(fileId: string): Promise<ReadableStream> {
+  public getDownloadFileStream(
+    fileId: string,
+    progress?: (totalBytes: number, downloadedBytes: number) => void
+  ): Promise<ReadableStream> {
+    const { bucketId, encryptionKey } = getSendAccountParameters();
+
     return downloadFile({
-      bucketId: process.env.REACT_APP_SEND_BUCKET_ID,
+      bucketId,
       fileId,
       creds: this.creds,
-      mnemonic: process.env.REACT_APP_SEND_ENCRYPTION_KEY,
+      mnemonic: encryptionKey,
       options: {
-        notifyProgress: (total, downloaded) => {
-          console.log('PROGRESS', downloaded / total);
-        },
+        notifyProgress: (totalBytes, downloadedBytes) => {
+          progress && progress(totalBytes, downloadedBytes);
+        }
       }
+    });
+  }
+
+  async uploadFile(
+    file: File, 
+    progress?: (totalBytes: number, uploadedBytes: number) => void
+  ): Promise<string> {
+    const { bucketId, user, pass, encryptionKey } = getSendAccountParameters();
+
+    let parts = 1;
+    const partSize = 100*1024*1024;
+    const minimumMultipartThreshold = 50*1024*1024;
+
+    if (file.size > minimumMultipartThreshold) {
+      parts = Math.ceil(file.size / partSize);
+    }
+
+    return uploadFile(bucketId, {
+      creds: { user, pass },
+      filecontent: file,
+      filesize: file.size,
+      mnemonic: encryptionKey,
+      progressCallback: (totalBytes, uploadedBytes) => {
+        progress && progress(totalBytes, uploadedBytes);
+      },
+      parts
     });
   }
 }
