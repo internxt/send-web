d83e8739cd58cf0109c7105428838694cd037aee
Alex Menor <alex.menor@icloud.com>
2022-06-22 16:34:31 +0200

    tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import HomeView from './views/HomeView';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+    <div>
         <HomeView></HomeView>
-      </header>
     </div>
   );
 }
