e0672d24b3448cf9f48e6873c639a67888e73ef0
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-06-23 15:47:51 +0200

    Brave download support

--- src/services/zip/FlatFolderZip.ts
+++ src/services/zip/FlatFolderZip.ts
@@ -1,38 +1,49 @@
 import streamSaver from 'streamsaver';
+import fileDownload from 'js-file-download';
 
-import { buildProgressStream } from '../../network/streams';
+import { binaryStreamToBlob, buildProgressStream } from '../../network/streams';
 import { loadWritableStreamPonyfill, createFolderWithFilesWritable, ZipStream } from "./Zip";
 
 type FlatFolderZipOpts = {
   abortController?: AbortController;
   progress?: (loadedBytes: number) => void;
 }
+function isBrave() {
+  const maybeBrave = (window.navigator as { brave?: any }).brave;
+
+  return maybeBrave != undefined && maybeBrave.isBrave.name == "isBrave";
+}
 export class FlatFolderZip {
   private finished!: Promise<void>;
   private zip: ZipStream;
+  private passThrough: ReadableStream<Uint8Array>; 
+  private folderName: string;
   private abortController?: AbortController;
 
   constructor(folderName: string, opts: FlatFolderZipOpts) {
+    this.folderName = folderName;
     this.zip = createFolderWithFilesWritable();
     this.abortController = opts.abortController;
 
-    const passThrough = opts.progress ?
+    this.passThrough = opts.progress ?
       buildProgressStream(this.zip.stream, opts.progress) :
       this.zip.stream;
 
     const isFirefox = navigator.userAgent.indexOf('Firefox') !== -1;
 
+    if (isBrave()) return;
+
     if (isFirefox) {
       loadWritableStreamPonyfill().then(() => {
         streamSaver.WritableStream = window.WritableStream;
 
-        this.finished = passThrough.pipeTo(
+        this.finished = this.passThrough.pipeTo(
           streamSaver.createWriteStream(folderName + '.zip'),
           { signal: opts.abortController?.signal }
         );
       });
     } else {
-      this.finished = passThrough.pipeTo(
+      this.finished = this.passThrough.pipeTo(
         streamSaver.createWriteStream(folderName + '.zip'),
         { signal: opts.abortController?.signal }
       );
@@ -52,9 +63,19 @@ export class FlatFolderZip {
   }
 
   async close(): Promise<void> {
-    if (this.abortController?.signal.aborted) return;
+    if (this.abortController?.signal.aborted) return; 
 
     this.zip.end();
+
+    if (isBrave()) {
+      console.log('is Brave');
+      return fileDownload(
+        await binaryStreamToBlob(this.passThrough),
+        `${this.folderName}.zip`,
+        'application/zip'
+      );
+    }
+  
     await this.finished;
   }
 
