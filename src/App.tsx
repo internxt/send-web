39e55fe03917bad8495a099747fcb74ff13daac5
Alex Menor <alex.menor@icloud.com>
2022-06-29 09:33:40 +0200

    scaffold DownloadView and add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import { Toaster } from "react-hot-toast";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RootDropzone from "./components/RootDropzone";
 import { FilesProvider } from "./contexts/Files";
+import DownloadView from "./views/DownloadView";
 import HomeView from "./views/HomeView";
 
 function App() {
   return (
-    <FilesProvider>
-      <RootDropzone>
-        <HomeView></HomeView>
-      </RootDropzone>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <FilesProvider>
+                <RootDropzone>
+                  <HomeView />
+                </RootDropzone>
+              </FilesProvider>
+            }
+          />
+          <Route path="/:shareId" element={<DownloadView />} />
+        </Routes>
+      </BrowserRouter>
       <Toaster position="bottom-center" />
-    </FilesProvider>
+    </>
   );
 }
 
---
+++ src/views/DownloadView.tsx
@@ -0,0 +1,9 @@
+import Layout from "../Layout";
+
+export default function DownloadView() {
+  return (
+    <Layout>
+      <p>Download view</p>
+    </Layout>
+  );
+}
