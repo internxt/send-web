4fb34acdb25fbb385b6ca24ddcc7fd43e972acd9
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-06-29 10:58:11 +0200

    upload files and generate token

--- src/services/network.service.ts
+++ src/services/network.service.ts
@@ -1,5 +1,7 @@
 import downloadFile from "../network/download";
 import { uploadFile } from "../network/upload";
+import axios, { AxiosRequestConfig } from 'axios';
+import { createHash } from 'crypto';
 
 interface NetworkCredentials {
   user: string;
@@ -54,7 +56,72 @@ export class NetworkService {
     });
   }
 
+  get encryptionKey(): string {
+    return process.env.REACT_APP_SEND_ENCRYPTION_KEY;
+  }
+
   async uploadFile(
+    file: File, 
+    opts?: {
+      progress?: (totalBytes: number, downloadedBytes: number) => void,
+      abortController?: AbortController
+    }
+  ): Promise<string> {
+    const { bucketId, user, pass, encryptionKey } = getSendAccountParameters();
+
+    let parts
+    const partSize = 100*1024*1024;
+    const minimumMultipartThreshold = 100*1024*1024;
+
+    if (file.size > minimumMultipartThreshold) {
+      parts = Math.ceil(file.size / partSize);
+    }
+
+    return uploadFile(bucketId, {
+      creds: { user, pass },
+      filecontent: file,
+      filesize: file.size,
+      mnemonic: encryptionKey,
+      progressCallback: (totalBytes, uploadedBytes) => {
+        opts?.progress?.(totalBytes, uploadedBytes);
+      },
+      parts,
+      abortController: opts?.abortController
+    });
+  }
+
+  async getShareToken(): Promise<string> {
+    // TODO: Move to SDK
+    const bucketId = process.env.REACT_APP_SEND_BUCKET_ID;
+    const operation = 'PULL';
+    const requestUrl = `${process.env.REACT_APP_NETWORK_URL}/buckets/${bucketId}/tokens`;
+
+    function sha256(input: Buffer): Buffer {
+      return createHash('sha256').update(input).digest();
+    }
+
+    const opts: AxiosRequestConfig = {
+      method: 'POST',
+      auth: {
+        username: process.env.REACT_APP_SEND_USER,
+        password: sha256(Buffer.from(process.env.REACT_APP_SEND_PASS)).toString('hex'),
+      },
+      url: requestUrl,
+      data: { operation }
+    };
+  
+    const res = await axios.request<{
+      bucket: string
+      encryptionKey: string,
+      id: string,
+      operation: 'PULL',
+      token: string;
+    }>(opts);
+
+    return res.data.token;
+  }
+
+  async uploadFileForShare(
     file: File, 
     opts?: {
       progress?: (totalBytes: number, downloadedBytes: number) => void,
