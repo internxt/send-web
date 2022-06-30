0d80d31efb965150ef0bc534710a332b93f09a39
Alex Menor <alex.menor@icloud.com>
2022-06-30 15:31:16 +0200

    list files in download

--- src/components/FileArea.tsx
+++ src/components/FileArea.tsx
@@ -1,9 +1,9 @@
-import { PlusCircle, X } from "phosphor-react";
+import { PlusCircle } from "phosphor-react";
 import { ChangeEvent, useContext, useRef } from "react";
 import { FilesContext } from "../contexts/Files";
-import bytes, { format } from "bytes";
-import { extension } from "mime-types";
+import { format } from "bytes";
 import { MAX_BYTES_PER_SEND } from "../constants";
+import FileList from "./FileList";
 
 export default function FileArea({
   className = "",
@@ -39,19 +39,14 @@ export default function FileArea({
       {fileContext.files.length === 0 && <Empty onClick={openFileExplorer} />}
       {fileContext.files.length !== 0 && (
         <>
-          <div
-            className={`flex-1 bg-gray-1 ${
+          <FileList
+            files={fileContext.files}
+            onRemoveFile={fileContext.removeFile}
+            className={`flex-1 bg-gray-1 py-3 px-5 ${
               scroll ? "overflow-y-auto" : ""
-            } py-3 px-5`}
-          >
-            {fileContext.files.map((file, i) => (
-              <Item
-                key={file.name}
-                file={file}
-                onRemove={() => fileContext.removeFile(i)}
-              />
-            ))}
-          </div>
+            }`}
+          />
+
           <div
             className="flex cursor-pointer select-none items-center bg-gray-1 px-5 py-2.5"
             onClick={openFileExplorer}
@@ -72,25 +67,6 @@ export default function FileArea({
   );
 }
 
-function Item({ file, onRemove }: { file: File; onRemove: () => void }) {
-  return (
-    <div className="group relative border-b border-gray-5 py-1.5">
-      <h1 className="truncate text-base text-gray-80">{file.name}</h1>
-      <div className="flex space-x-1.5 text-xs text-gray-50">
-        <p>{bytes.format(file.size)}</p>
-        <p className="font-bold text-gray-30">·</p>
-        <p>{extension(file.type)}</p>
-      </div>
-      <button
-        className="absolute right-0 top-0 flex h-full items-center justify-end bg-gradient-to-r from-transparent via-gray-1 to-gray-1 pl-10 lg:hidden lg:group-hover:flex"
-        onClick={onRemove}
-      >
-        <X size={20} className="text-gray-80" />
-      </button>
-    </div>
-  );
-}
-
 function Empty({ onClick }: { onClick: () => void }) {
   const maxBytesPerSendDisplay = format(MAX_BYTES_PER_SEND);
   return (
@@ -102,7 +78,7 @@ function Empty({ onClick }: { onClick: () => void }) {
         <PlusCircle size={48} className="text-primary" weight="fill" />
         <div className="ml-2">
           <h1 className="text-2xl font-medium text-gray-80">Upload files</h1>
-          <h2 className="text-sm text-gray-50 -mt-0.5">
+          <h2 className="-mt-0.5 text-sm text-gray-50">
             Up to {maxBytesPerSendDisplay}
           </h2>
         </div>
