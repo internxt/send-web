aa926d23f638e98ea5e676afa15090d51555b63e
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-07-18 12:01:26 +0200

    refactor(download): move send item entity to /models

---
+++ src/models/SendItem.ts
@@ -0,0 +1,12 @@
+export interface SendItem {
+  id: string;
+  name: string;
+  type: string;
+  linkId: string;
+  networkId: string;
+  encryptionKey: string;
+  size: number;
+  createdAt: Date;
+  updatedAt: Date;
+  version: number;
+}
