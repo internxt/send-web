9acf9155a9f4908cd3ae8b9401885e31e1882875
Larry <larry@internxt.com>
2022-10-14 12:07:56 +0200

    refactored FileList

--- src/components/FileList.tsx
+++ src/components/FileList.tsx
@@ -1,31 +1,23 @@
 import bytes from "bytes";
 import { X } from "phosphor-react";
-import { FileWithPath } from "../contexts/Files";
+import { SendItemData } from "../models/SendItem";
 
-type ItemDetails = Pick<FileWithPath, "name" | "size" | "type"> & {
-  path?: string;
-};
-
-export default function FileList({
+export default function ItemList({
   className = "",
-  files,
-  onRemoveFile,
+  items,
+  onRemoveItem,
 }: {
   className?: string;
-  files: ItemDetails[];
-  onRemoveFile?: (path: string) => void;
+  items: SendItemData[];
+  onRemoveItem?: (item: SendItemData) => void;
 }) {
   return (
     <div className={`${className}`}>
-      {files.map((file) => (
+      {items.map((item) => (
         <Item
-          key={file.name}
-          file={file}
-          onRemove={
-            onRemoveFile
-              ? () => onRemoveFile(file?.path ?? file?.name)
-              : undefined
-          }
+          key={item.id}
+          item={item}
+          onRemove={onRemoveItem}
         />
       ))}
     </div>
@@ -33,26 +25,37 @@ export default function FileList({
 }
 
 function Item({
-  file,
+  item,
   onRemove,
 }: {
-  file: ItemDetails;
-  onRemove?: () => void;
+  item: SendItemData;
+  onRemove?: (item: SendItemData) => void;
 }) {
-  const [extension, ...rest] = file.name.split(".").reverse();
+  const extension = item.file?.name.includes('.') && item.file?.name.split(".").pop();
 
   return (
     <div className="group relative border-b border-gray-5 py-2.5">
-      <h1 className="truncate text-base text-gray-80">{file.name}</h1>
+      <h1 className="truncate text-base text-gray-80">{item.name}</h1>
       <div className="flex space-x-1.5 text-xs text-gray-50">
-        <p>{bytes.format(file.size)}</p>
-        <p className="font-bold text-gray-30">·</p>
-        <p>{extension}</p>
+        {item.isFolder &&
+          <>
+            <p>Folder</p>
+            <p className="font-bold text-gray-30">·</p>
+            {item.countFiles && <>
+              <p>{item.countFiles} {item.countFiles === 1 ? 'file' : 'files'}</p>
+              <p className="font-bold text-gray-30">·</p>
+            </>}
+          </>}
+        <p>{bytes.format(item.size)}</p>
+        {extension && <>
+          <p className="font-bold text-gray-30">·</p>
+          <p>{extension}</p>
+        </>}
       </div>
       {onRemove && (
         <button
           className="absolute right-0 top-0 flex h-full items-center justify-end bg-gradient-to-r from-transparent via-gray-1 to-gray-1 pl-12 lg:hidden lg:group-hover:flex"
-          onClick={onRemove}
+          onClick={() => onRemove(item)}
         >
           <X size={20} className="text-gray-80" />
         </button>
