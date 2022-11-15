30d283bb026d58d5b06d03992d57f02e02dce6b2
Fabio Espinosa <fa.espinosa10@uniandes.edu.co>
2022-11-15 14:47:21 +0100

    fix(base-url): specific assets without base-url

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -2,7 +2,7 @@
 
 declare namespace NodeJS {
   interface ProcessEnv {
-    NODE_ENV: 'development' | 'production' | 'test';
+    NODE_ENV: "development" | "production" | "test";
     PUBLIC_URL: string;
     REACT_APP_API_URL: string;
     REACT_APP_CRYPTO_SECRET: string;
@@ -14,5 +14,6 @@ declare namespace NodeJS {
     REACT_APP_SEND_PASS: string;
     REACT_APP_SEND_ENCRYPTION_KEY: string;
     REACT_APP_SEND_BUCKET_ID: string;
+    REACT_APP_BASE_URL: string;
   }
 }
