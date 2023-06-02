f1d8dcd87563c861fa8ce0ed59cc97bdcb8f4063
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2023-06-02 12:34:22 +0200

    Add arrow icon and smoth animation

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 
 import Card from "./components/Card";
 import Navbar from "./components/Navbar/Navbar";
+import { ArrowCircleDown, ArrowDown } from "phosphor-react";
 
 const urlPrefix = process.env.REACT_APP_BASE_URL || "";
 export default function Layout({ children }: { children: ReactNode }) {
@@ -44,9 +45,9 @@ export default function Layout({ children }: { children: ReactNode }) {
 
       <Navbar />
 
-      <div className="relative min-h-0 flex-1 lg:py-0 lg:pb-32 lg:pt-24">
-        <div className="relative mx-auto flex h-full max-w-screen-xl flex-row items-center justify-center">
-          <div className="flex h-full w-full flex-row items-center justify-start">
+      <div className="relative min-h-0 flex-1 lg:py-0  lg:pt-24">
+        <div className="relative mx-auto flex h-full max-w-screen-xl flex-col items-center justify-center">
+          <div className="flex h-full w-full flex-row items-center justify-start lg:pb-32">
             <Card className="flex flex-shrink-0 flex-col">{children}</Card>
 
             <div
@@ -66,6 +67,9 @@ export default function Layout({ children }: { children: ReactNode }) {
                 Select the files you want to encrypt and send by link or email.
               </p>
             </div>
+          </div>
+          <div className="absolute bottom-12 hidden lg:flex">
+            <ArrowCircleDown size={32} className="animate-bounce text-white" />
           </div>
         </div>
       </div>
