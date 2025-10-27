1cf97aa727739566baa4664287dbf2a752dd1044
larryrider <larryrider@users.noreply.github.com>
2025-10-27 14:40:56 +0100

    feat: add environment variable service and tests for loading env variables

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,18 +3,20 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: "development" | "production" | "test";
-    PUBLIC_URL: string;
-    REACT_APP_SEND_API_URL: string;
-    REACT_APP_CRYPTO_SECRET: string;
-    REACT_APP_CRYPTO_SECRET2: string;
-    REACT_APP_NETWORK_URL: string;
-    REACT_APP_PROXY: string;
 
     REACT_APP_SEND_USER: string;
     REACT_APP_SEND_PASS: string;
+    REACT_APP_MAGIC_IV: string;
+    REACT_APP_MAGIC_SALT: string;
+    REACT_APP_GA_ID: string;
+    REACT_APP_NETWORK_URL: string;
     REACT_APP_SEND_ENCRYPTION_KEY: string;
     REACT_APP_SEND_BUCKET_ID: string;
-    REACT_APP_BASE_URL: string;
+    REACT_APP_STRIPE_PK: string;
+    REACT_APP_STRIPE_TEST_PK: string;
+    REACT_APP_SENTRY_DSN: string;
+    REACT_APP_SEND_API_URL: string;
+    REACT_APP_RECAPTCHA_V3: string;
   }
 }
 
---
+++ src/services/env.service.ts
@@ -0,0 +1,31 @@
+const envService = {
+  getVariable,
+};
+const variableList = {
+  nodeEnv: 'NODE_ENV',
+  sendUser: 'REACT_APP_SEND_USER',
+  sendPass: 'REACT_APP_SEND_PASS',
+  magicIv: 'REACT_APP_MAGIC_IV',
+  magicSalt: 'REACT_APP_MAGIC_SALT',
+  gaId: 'REACT_APP_GA_ID',
+  networkUrl: 'REACT_APP_NETWORK_URL',
+  sendEncryptionKey: 'REACT_APP_SEND_ENCRYPTION_KEY',
+  sendBucketId: 'REACT_APP_SEND_BUCKET_ID',
+  stripePublicKey: 'REACT_APP_STRIPE_PK',
+  stripeTestPublicKey: 'REACT_APP_STRIPE_TEST_PK',
+  sentryDsn: 'REACT_APP_SENTRY_DSN',
+  sendApiUrl: 'REACT_APP_SEND_API_URL',
+  recaptchaV3: 'REACT_APP_RECAPTCHA_V3',
+};
+
+
+function getVariable(variable: keyof typeof variableList): string {
+  const envKey = variableList[variable];
+  if (!envKey) {
+    throw new Error(`Unknown variable name: "${variable}"`);
+  }
+  const value = import.meta.env[envKey];
+  return value;
+}
+
+export default envService;
