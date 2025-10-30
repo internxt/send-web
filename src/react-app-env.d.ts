67233a2e5cc7b70c284e3deb2195736c85341787
larryrider <larryrider@users.noreply.github.com>
2025-10-30 13:08:18 +0100

    feat: remove unused rudder analytics

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -27,6 +27,4 @@ interface Window {
   };
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   gtag: any;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  rudderanalytics: any;
 }
