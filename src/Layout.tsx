3b9da5a8404663559c3a100f70f23ba6647e740f
larryrider <larryrider@users.noreply.github.com>
2025-11-12 17:56:03 +0100

    fix: update Item interface to fix mobile view and adjust layout styles

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -206,11 +206,11 @@ export default function Layout({
             className="relative flex h-full max-w-screen-xl flex-col items-center justify-center md:px-10
             xl:mx-auto xl:px-0 3xl:max-w-full"
           >
-            <div className="relative h-full w-full items-center justify-start space-x-20 lg:pb-32 3xl:translate-x-50">
+            <div className="relative flex h-full w-full items-center justify-start lg:pb-32 3xl:translate-x-50">
               <div className="relative flex h-full w-full items-center lg:w-max">
                 <Card className="relative z-30 flex shrink-0 flex-col pt-12 md:pt-0">{children}</Card>
               </div>
-              <div className="hidden text-white  lg:block">{BgLoop(item.text, ctaRef)}</div>
+              <div className="hidden text-white lg:block ml-80">{BgLoop(item.text, ctaRef)}</div>
             </div>
             {hasContentBelow && (
               <div className="absolute bottom-12 hidden lg:flex">
